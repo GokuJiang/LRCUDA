b702270a17ac905d34313dc6ad1c02550ed82933
goku <jym775096422@163.com>
2017-09-15 11:21:32 +0800

    test: SmeiExh test

--- LRCUDA_DEMO/MultipleCluster.R
+++ LRCUDA_DEMO/MultipleCluster.R
@@ -17,12 +17,11 @@ binaryzation <- function(str) {
 mat <- matrix(unlist(data), ncol=2001, byrow=TRUE)
 y <- mat[2:63,1]
 y <- unlist(lapply(y, function(i) binaryzation(i)))
-print(typeof(y))
 x = matrix(as.numeric(mat[2:63,2:2001]),ncol=2000,byrow=TRUE)
 
 #result <- LRCUDA(x,y,error.threshhold = 62,num.comb =3, device.id = c(0,1,2,3,0,1,2,3,0,1,2,3))
-#result <- SemiExh(x, y, error.threshhold = 30, fold = 5, device.id = c(0,1,2,3,0,1,2,3,0,1,2,3), cl = NULL)
-#save(result, file = "MultipleClusterResult.RData")
+result <- SemiExh(x, y, error.threshhold = 15, fold = 5, device.id = c(0,1,2,3,0,1,2,3,0,1,2,3), cl = NULL)
+save(result, file = "MultipleClusterResult.RData")
 
 
 
