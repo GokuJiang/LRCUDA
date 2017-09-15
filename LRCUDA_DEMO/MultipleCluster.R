2e3ae726f21860c1dd5e1977e1626cacac338aef
goku <jym775096422@163.com>
2017-09-15 13:07:41 +0800

    test

--- LRCUDA_DEMO/MultipleCluster.R
+++ LRCUDA_DEMO/MultipleCluster.R
@@ -2,6 +2,8 @@ library("LRCUDA")
 library("snow")
 load("~/LRCUDA/LRCUDA_DEMO/Colon.Rdata")
 
+
+
 binaryzation <- function(str) {
     
     flag <- 0
@@ -18,9 +20,12 @@ mat <- matrix(unlist(data), ncol=2001, byrow=TRUE)
 y <- mat[2:63,1]
 y <- unlist(lapply(y, function(i) binaryzation(i)))
 x = matrix(as.numeric(mat[2:63,2:2001]),ncol=2000,byrow=TRUE)
-
 #result <- LRCUDA(x,y,error.threshhold = 62,num.comb =3, device.id = c(0,1,2,3,0,1,2,3,0,1,2,3))
-result <- SemiExh(x, y, error.threshhold = 15, fold = 5, device.id = c(0,1,2,3,0,1,2,3,0,1,2,3), cl = NULL)
-save(as.martix(result), file = "MultipleClusterResult.txt")
+
+result <- SemiExh(x, y, error.threshhold = 20, fold = 5, device.id = c(0,1,2,3,0,1,2,3,0,1,2,3), cl = NULL)
+
+save(result, file = "MultipleClusterResult.RData")
+
 print(result)
+
 
