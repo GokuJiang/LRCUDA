2d6db5939bfb84951e3b1f8a42cc7c19d6a33461
Goku <jym775096422@163.com>
2017-11-06 18:53:14 +0800

    test

--- LRCUDA_DEMO/MultipleCluster.R
+++ LRCUDA_DEMO/MultipleCluster.R
@@ -28,15 +28,17 @@ print(row.n)
 print(column.n)
 
 x <- colon[1:row.n,2:column.n]
-print(dim(x))
 
 x <- matrix(as.numeric(unlist(x)),ncol = row.n,byrow=TRUE)
 
 y <- colon[1:1,2:column.n]
+
 y <- lapply(y,function(i) binaryzation(i))
+
 y <- matrix(unlist(y),ncol = 1,byrow=TRUE)
+
 write.csv(x, 'Colon_X.csv')
 write.csv(y, 'Colon_y.csv')
 
-#result <- SemiExh(x, y,n.comb = 3, error.threshhold = 50, device.id = gpu.ids, cl =NULL)
-#print(result)
+result <- SemiExh(x, y,n.comb = 3, error.threshhold = 50, device.id = gpu.ids, cl=NULL)
+print(result)
