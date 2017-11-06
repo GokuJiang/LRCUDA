b247fbbde498ef19eaa2df84df5f58131db8601c
Goku <jym775096422@163.com>
2017-11-06 18:59:34 +0800

    feat:check data

--- LRCUDA_DEMO/MultipleCluster.R
+++ LRCUDA_DEMO/MultipleCluster.R
@@ -35,7 +35,7 @@ y <- colon[1:1,2:column.n]
 
 y <- lapply(y,function(i) binaryzation(i))
 
-y <- list(unlist(y),ncol = n.column.n-1,byrow=TRUE)
+y <- list(unlist(y),ncol = column.n-1,byrow=TRUE)
 
 write.csv(x, 'Colon_X.csv')
 write.csv(y, 'Colon_y.csv')
