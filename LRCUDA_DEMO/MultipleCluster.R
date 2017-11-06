c870001045b34fa136c614b9afc212b827c2d747
beefOnion <jym775096422@163.com>
2017-11-06 07:07:49 +0000

    test

--- LRCUDA_DEMO/MultipleCluster.R
+++ LRCUDA_DEMO/MultipleCluster.R
@@ -16,7 +16,7 @@ binaryzation <- function(str) {
 }
 
 
-colon<-read.table('Colon.csv',sep=",",as.is=FALSE)
+colon<-read.csv('Colon.csv')
 
 shape <- dim(colon)
 
@@ -31,7 +31,7 @@ x <- matrix(as.numeric(unlist(x)),ncol = row.n-1,byrow=TRUE)
 
 y <- colon[1,2:colon.n]
 y <- lapply(y,function(i) binaryzation(i))
-y <- matrix(as.numeric(unlist(y)),ncol = 1,byrow=TRUE)
+y <- matrix(unlist(y),ncol = 1,byrow=TRUE)
 write.csv(x, 'Colon_X.csv')
 write.csv(y, 'Colon_y.csv')
 
