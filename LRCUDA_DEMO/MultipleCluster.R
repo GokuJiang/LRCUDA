a57238ed5315badbc39be631974a2cf5c685f097
Goku <jym775096422@163.com>
2017-11-06 16:26:32 +0800

    feat:test

--- LRCUDA_DEMO/MultipleCluster.R
+++ LRCUDA_DEMO/MultipleCluster.R
@@ -32,9 +32,9 @@ print(dim(x))
 
 x <- matrix(as.numeric(unlist(x)),ncol = row.n,byrow=TRUE)
 
-#y <- colon[2:2,2:colon.n]
-#y <- lapply(y,function(i) binaryzation(i))
-#y <- matrix(unlist(y),ncol = 1,byrow=TRUE)
+y <- colon[1:1,2:colon.n]
+y <- lapply(y,function(i) binaryzation(i))
+y <- matrix(unlist(y),ncol = 1,byrow=TRUE)
 write.csv(x, 'Colon_X.csv')
 write.csv(y, 'Colon_y.csv')
 
