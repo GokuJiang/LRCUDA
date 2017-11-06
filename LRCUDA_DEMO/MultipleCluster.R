dfd7e5b9a37e5789fdf552f80fa9fb1b9e18f0a6
Goku <jym775096422@163.com>
2017-11-06 16:17:55 +0800

    feat:test

--- LRCUDA_DEMO/MultipleCluster.R
+++ LRCUDA_DEMO/MultipleCluster.R
@@ -20,8 +20,8 @@ colon<-read.csv('Colon.csv')
 
 shape <- dim(colon)
 
-row.n <- shape[1]
-column.n <- shape[2]
+row.n <- shape[1] //2000
+column.n <- shape[2] //62
 
 gpu.ids <- getGPUIds()
 print(row.n)
@@ -34,11 +34,11 @@ print(dim(x))
 
 x <- matrix(as.numeric(unlist(x)),ncol = row.n,byrow=TRUE)
 
-y <- colon[1,2:colon.n]
-y <- lapply(y,function(i) binaryzation(i))
-y <- matrix(unlist(y),ncol = 1,byrow=TRUE)
+#y <- colon[2:2,2:colon.n]
+#y <- lapply(y,function(i) binaryzation(i))
+#y <- matrix(unlist(y),ncol = 1,byrow=TRUE)
 write.csv(x, 'Colon_X.csv')
 write.csv(y, 'Colon_y.csv')
 
-result <- SemiExh(x, y,n.comb = 3, error.threshhold = 50, device.id = gpu.ids, cl =NULL)
-print(result)
+#result <- SemiExh(x, y,n.comb = 3, error.threshhold = 50, device.id = gpu.ids, cl =NULL)
+#print(result)
