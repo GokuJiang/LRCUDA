e5548a4d26b5e43cbb40cd24e1f401984deaba3c
Goku <jym775096422@163.com>
2017-11-08 20:11:26 +0800

    LR Mutiple GPU

--- FSCUDA/R/LRMultipleGPU.R
+++ FSCUDA/R/LRMultipleGPU.R
@@ -5,24 +5,17 @@
 
 LRMultipleGPU <- function(para){
     x <- t(para$x)
-    print(x)
     y <- para$y
-    print(y)
     n.comb <- as.integer(para$n.comb)
-    print(n.comb)
     error.threshhold <- as.integer(para$error.threshhold)
-    print(error.threshhold)
     fold <- as.integer(para$fold)
-    print(fold)
     device.id <- as.integer(para$device.id)
-    print(device.id)
     start <- as.integer(para$start)
-    print(start)
     stop <- as.integer(para$start)
-    print(stop)
     
-     result <- .Call("LRCUDA", x, y, n.comb, error.threshhold, fold, device.id, start, stop)
-     print(result)
-     return(t(matrix(result, nrow = n.comb + 1)))
+    result <- .Call("LRCUDA", x, y, n.comb, error.threshhold, fold, device.id, start, stop)
+    print('LM1') 
+    print(result)
+    return(t(matrix(result, nrow = n.comb + 1)))
      
 }
