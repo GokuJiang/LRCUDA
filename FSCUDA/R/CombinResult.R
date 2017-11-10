4e79afe274ea7c12288e25ff787a408ec7ae03b1
Goku <jym775096422@163.com>
2017-11-10 19:36:12 +0800

    no message

--- FSCUDA/R/CombinResult.R
+++ FSCUDA/R/CombinResult.R
@@ -1,20 +1,12 @@
-#combine analysis result from cluster.
-#store the result as data frame type.
 
-#' Result
-#' @param result result of LRCUDA
-#' @export
+
 combineResult <- function(result){
-
-    #result is a list
-    
     result <- do.call(rbind, result)
     result <- as.data.frame(result)
-
     features.num <- ncol(result) - 1
     header.names <- paste("feature", 1:features.num, sep=".")
-    header.names <- c(header.names, "error")
+    header.names <- c(header.names, "logloss")
     names(result) <- header.names
     return(result)
-
+    
 }
